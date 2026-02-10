164bdaddb1aa5d7c6e3be8bfd6a788939c641989
yu-Yuming <yuming.yuu@gmail.com>
2026-02-10 23:09:12 +0900

    unlock get_world_size()

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+clear()
 while True:
-    if can_harvest():
-        harvest()
-        move(North)
+	plant(Entities.Bush)
+    for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if can_harvest():
+				harvest()
+			move(North)
+		move(East)
+		
