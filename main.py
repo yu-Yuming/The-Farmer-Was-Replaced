09d7b3f71ff73b8519b1e6662b4da75adbe0ec67
yu-Yuming <78905337+yu-Yuming@users.noreply.github.com>
2026-02-11 00:27:19 +0900

    unlock carrot

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-clear()
 while True:
-	plant(Entities.Bush)
-	for i in range(get_world_size()):
-		for j in range(get_world_size()):
-			if can_harvest():
-				harvest()
-			move(North)
-		move(East)
-		
+    for i in range(get_world_size()):
+        for j in range(get_world_size()):
+            plant(Entities.Bush)
+            if can_harvest():
+                harvest()
+                till()
+                plant(Entities.Carrot)
+            move(North)
+        move(East)
